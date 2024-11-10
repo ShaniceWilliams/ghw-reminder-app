e0965e74c6551dadc9cb9cbac26ee3d197049e4b
Shanice Williams <shanice.williams@live.co.uk>
2024-11-10 19:45:11 +0000

    feat: added _layout, completed, addreminder routes

---
+++ app/_layout.tsx
@@ -0,0 +1,45 @@
+import { Tabs, useRouter } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "../theme";
+import { StyleSheet } from "react-native";
+
+export default function TabsLayout() {
+  const router = useRouter();
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.blue100,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Reminders",
+          tabBarIcon: ({ color }) => <Feather name="list" size={24} color={color} />,
+          headerRight: () => (
+            <Feather
+              name="plus-circle"
+              size={24}
+              color={theme.colors.green100}
+              style={style.addReminders}
+              onPress={() => router.push("addReminder")}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="completed"
+        options={{
+          title: "Completed Reminders",
+          tabBarIcon: ({ color }) => <Feather name="check-circle" size={24} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
+
+const style = StyleSheet.create({
+  addReminders: {
+    marginRight: 20,
+  },
+});
--- theme.ts
+++ theme.ts
@@ -1,5 +1,8 @@
 export const theme = {
   colors: {
     white: "#ffffff",
+    blue100: "#0284cf",
+    blue90: "##0298ee",
+    green100: "#02cf8c"
   },
 };
